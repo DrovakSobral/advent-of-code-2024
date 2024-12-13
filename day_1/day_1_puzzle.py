b399ec8eec518f74307ecd60c84c72595c193738
João Manoel Sobral da Costa <30218806+DrovakSobral@users.noreply.github.com>
2024-12-13 20:57:12 -0300

    Day 1 Full Puzzle Solution

--- day_1/day_1_puzzle.py
+++ day_1/day_1_puzzle.py
@@ -90,24 +90,84 @@ To begin, get your puzzle input (day_1_puzzle_input_1.txt).
 
 file = open('.\\day_1\\day_1_puzzle_input')
 raw_input = file.read().split()
-list_1 = []
-list_2 = []
+raw_list_1 = []
+raw_list_2 = []
 for x in range(len(raw_input)):
     if x == 0 or x % 2 == 0:
-        list_1.append(raw_input[x])
+        raw_list_1.append(raw_input[x])
     else:
-        list_2.append(raw_input[x])
-
-list_1.sort()
-list_2.sort()
+        raw_list_2.append(raw_input[x])
+raw_list_1.sort()
+raw_list_2.sort()
+list_1 = [int(x) for x in raw_list_1]
+list_2 = [int(x) for x in raw_list_2]
 
 distance = 0
 for x,y in zip(list_1, list_2):
-    x = int(x)
-    y = int(y)
     if x>y:
         distance += x-y
     elif x<y:
         distance += y-x
 
-print(distance)
+print('Total Distance: ', distance)
+
+'''
+--- Part Two ---
+
+Your analysis only confirmed what everyone feared: the two lists of 
+location IDs are indeed very different.
+
+Or are they?
+
+The Historians can't agree on which group made the mistakes or how to read 
+most of the Chief's handwriting, but in the commotion you notice an 
+interesting detail: a lot of location IDs appear in both lists! Maybe the 
+other numbers aren't location IDs at all but rather misinterpreted 
+handwriting.
+
+This time, you'll need to figure out exactly how often each number from the 
+left list appears in the right list. Calculate a total similarity score by 
+adding up each number in the left list after multiplying it by the number 
+of times that number appears in the right list.
+
+Here are the same example lists again:
+
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+
+For these example lists, here is the process of finding the similarity score:
+
+     - The first number in the left list is 3. It appears in the right list 
+       three times, so the similarity score increases by 3 * 3 = 9.
+     - The second number in the left list is 4. It appears in the right list 
+       once, so the similarity score increases by 4 * 1 = 4.
+     - The third number in the left list is 2. It does not appear in the 
+       right list, so the similarity score does not increase (2 * 0 = 0).
+     - The fourth number, 1, also does not appear in the right list.
+     - The fifth number, 3, appears in the right list three times; the 
+       similarity score increases by 9.
+     - The last number, 3, appears in the right list three times; 
+       the similarity score again increases by 9.
+
+So, for these example lists, the similarity score at the end of this 
+process is 31 (9 + 4 + 0 + 0 + 9 + 9).
+
+Once again consider your left and right lists. What is their similarity 
+score?
+
+'''
+similarity_score = 0
+for x in list_1:
+    number_of_repetitions = 0
+    for y in list_2:
+        if x == y:
+            number_of_repetitions += 1
+
+    if number_of_repetitions>0:
+        similarity_score += x * number_of_repetitions
+
+print('Similarity Score: ', similarity_score)
